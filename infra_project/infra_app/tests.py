9a0c86d9209b29efc8fb00ecb52bcf9d036829fd
Viktor Dordzhiev <victor.dorjieff@yandex.ru>
2022-01-09 18:46:05 +0300

    add test with flake8-test3

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -21,4 +21,4 @@ class StaticPagesURLTests(TestCase):
         self.assertContains(response, 'У меня получилось!')
 
         response = self.guest_client.get('/second_page/')
-        self.assertContains(response, 404)
+        self.assertContains(response, 200)
